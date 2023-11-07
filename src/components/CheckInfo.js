cc98ed8b5dfa8dbbbc91cd56e0f06bf2a315b30c
HugoDMiranda <hdmirandab@hotmail.com>
2023-11-06 21:46:48 -0500

    CheckInfo component style update

--- src/components/CheckInfo.js
+++ src/components/CheckInfo.js
@@ -1,18 +1,24 @@
 import React from "react";
+import "../styles/styles-components/CheckInfo.css";
 
 function CheckInfo() {
   return (
     <div className="check-container-info">
-      <div>
+      <div className="check-container-info-fly">
         <h3>Vuelo XXX</h3>
-        <p>De Bogoto a Medellin</p>
+        <p>Bogota a Medellin</p>
         <p>1 hora aprox</p>
         <p>Puerta de embarque a las XX:XX</p>
         <p>Finaliza a las XX:XX</p>
       </div>
-      <div>
-        <h4>Piloto X</h4>
-        <p>Asafatas xxx</p>
+      <div className="check-container-info-team">
+        <h4>Our team</h4>
+        <h3>Piloto X</h3>
+        <div className="check-container-info-team-help">
+          <p>Asafatas</p>
+          <p>Asafatas</p>
+          <p>Asafatas</p>
+        </div>
       </div>
     </div>
   );
