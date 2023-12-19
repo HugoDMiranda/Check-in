8a602091d5280fce138124af0ecf7fc53f54c24e
HugoDMiranda <hdmirandab@hotmail.com>
2023-12-19 00:19:04 -0500

    Flights style added

--- src/views/Flights.js
+++ src/views/Flights.js
@@ -1,11 +1,30 @@
-import React from "react";
+import Navbar from "../components/Navbar";
+import "../styles/Flights.css";
 
 function Flights() {
   return (
-    <div>
-      Flights
-      <div>s</div>
-    </div>
+    <section>
+      <Navbar />
+      <div className="flights-container">
+        <details name="flights" className="flights-container-info domestic">
+          <summary>Domestic Flights</summary>
+          <p>
+            Pencilgon desvela su meticuloso plan para derrotar a Wezermon, pero,
+            antes, Sunraku y Katsuo deberán entrenarse para subir de nivel.
+          </p>
+        </details>
+        <details
+          name="flights"
+          className="flights-container-info international"
+        >
+          <summary>International flights</summary>
+          <p>
+            Pencilgon desvela su meticuloso plan para derrotar a Wezermon, pero,
+            antes, Sunraku y Katsuo deberán entrenarse para subir de nivel.
+          </p>
+        </details>
+      </div>
+    </section>
   );
 }
 
