c1d2828aee4bd8496097a86a4d4ca44d83f945c9
HugoDMiranda <hdmirandab@hotmail.com>
2023-12-19 15:11:47 -0500

    background style update

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,11 +5,10 @@ function Logo({ logoLoading }) {
   return (
     <div className="logo-container">
       <img
-        src="img/check_in.png"
+        src="img/airplane.png"
         alt="logo-plane"
         className={logoLoading ? logoLoading : `logo-plane`}
       />
-      <img className="name" src="img/check_text.png" alt="check_text" />
     </div>
   );
 }
